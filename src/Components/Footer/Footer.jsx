8430ade95e59529bc7c70a9dafec6967dae5cc7a
bushrafatima <bushrafatima176@gmail.com>
2021-02-28 19:05:18 +0500

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+const Footer = () => {
+    return (
+        <div>
+            <footer className="page-footer transparent">
+                <div className="container">
+                </div>
+                <div className="footer-copyright grey darken-3">
+                    <div className="container center-align">
+                        &copy; 2021 Campus Recruitment System <br />
+                      
+                    </div>
+                </div>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
